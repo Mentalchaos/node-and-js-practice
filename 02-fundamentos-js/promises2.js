3d1d9442df5f36597c86a859f4460deac313b3ce
Jean Pierre Vera <jvera@bs2.cl>
2019-07-22 19:57:00 -0400

    added second file for promises

---
+++ 02-fundamentos-js/promises2.js
@@ -0,0 +1,57 @@
+const empleados = [{
+    id: 1,
+    nombre: 'Jim Raynor'
+},
+{
+    id: 2,
+    nombre: 'Fenix'
+},
+{
+    id: 3,
+    nombre: 'Zeratul'
+}
+];
+
+const salarios = [{
+    id: 1,
+    salario: 3000
+},
+{
+    id: 2,
+    salario: 2000
+}
+];
+
+const getSalario = empleado => {
+    return new Promise((resolve,reject) => {
+        const money = salarios.find(salario => salario.id === empleado);
+        
+        if(!money){
+            reject(console.log(`No existe un salario con id ${empleado}`))
+        } else {
+            resolve({
+                nombre: empleados[empleado].nombre,
+                salario: money.salario
+            });
+        }
+    });
+};
+
+const getEmpleado = id => {
+    return new Promise ( (resolve, reject) => {
+        const empleadoDB = empleados.find(empleado => empleado.id === id);
+        !empleadoDB ? reject(`No existe un empleado con el id ${id}`) : resolve(empleadoDB);
+    })
+}
+
+getEmpleado(2).then(empleado => {
+    console.log('empleado', empleado);
+}, err => {
+    console.log(err)
+});
+
+getSalario(1).then(salario => {
+    console.log(`Salario de ${salario.nombre} es de ${salario.salario}`);
+}, err => {
+    console.log(err);
+});
